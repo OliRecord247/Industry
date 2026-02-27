9eb7694fcbf30dd582cefb0a7be7d3d54044cc29
Oliver <oliver.verdesseldonck@gmail.com>
2026-02-27 12:38:37 +0100

    Forgotten mapping of machine endpoints

---
+++ Industry.Api/Endpoints/Machines/MachinesEndpointsExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Industry.Api.Endpoints.Machines;
+
+public static class MachinesEndpointsExtensions
+{
+    public static IEndpointRouteBuilder MapMachineEndpoints(this IEndpointRouteBuilder app)
+    {
+        app.MapCreateMachineJob();
+        return app;
+    }
+}
