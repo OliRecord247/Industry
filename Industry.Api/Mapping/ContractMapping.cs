533f2cd6f8196525af20b0129f7d14db2237307a
Oliver <oliver.verdesseldonck@gmail.com>
2026-02-27 12:36:49 +0100

    Making create job contract and mapping for the API

---
+++ Industry.Api/Mapping/ContractMapping.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+using Industry.Contracts.requests;
+using Industry.Domain.Messaging;
+
+namespace Industry.Api.Mapping;
+
+public static class ContractMapping
+{
+    public static MachineJob MapToMachineJob(this CreateMachineJobRequest request)
+    {
+        return new MachineJob
+        {
+            MachineId = request.MachineId,
+            Command = request.Action,
+            ScheduledStart = DateTime.Parse(
+                request.ScheduledTime, 
+                CultureInfo.InvariantCulture,
+                DateTimeStyles.AdjustToUniversal | DateTimeStyles.AssumeUniversal)
+        };
+    }
+}
---
+++ Industry.Contracts/requests/CreateMachineJobRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Industry.Contracts.requests;
+
+public class CreateMachineJobRequest
+{
+    public required string MachineId { get; init; }
+    public required string ScheduledTime { get; init; }
+    public required string Action { get; init; }
+    public required bool IsProcessed { get; init; } = false;
+}
