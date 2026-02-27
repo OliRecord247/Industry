48b003f19d82b752b5353d60f66ac0388b6bacb2
Oliver <oliver.verdesseldonck@gmail.com>
2026-02-27 01:51:31 +0100

    Adding services in program file via extended service method

--- Industry.Api/Program.cs
+++ Industry.Api/Program.cs
@@ -1,8 +1,14 @@
+using Industry.Application;
+using Industry.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddApplication()
+    .AddInfrastructure("Host=localhost;Port=5433;Database=timetable_db;Username=admin;Password=secure_password");
 
 var app = builder.Build();
 
