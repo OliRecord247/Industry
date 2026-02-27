3ce338512c47f89c8313eca8305f3ab610058416
Oliver <oliver.verdesseldonck@gmail.com>
2026-02-27 01:49:27 +0100

    Dependency injection of repos and background services

---
+++ Industry.Application/DependencyInjection.cs
@@ -0,0 +1,13 @@
+﻿using Industry.Application.Workers;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Industry.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddHostedService<TimetableWorker>();
+        return services;
+    }
+}
---
+++ Industry.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using Industry.Domain.Messaging;
+using Industry.Infrastructure.Data;
+using Industry.Infrastructure.Messaging;
+using Industry.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Industry.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, string connectionString)
+    {
+        services.AddDbContext<TimetableDbContext>(options =>
+            options.UseNpgsql(connectionString));
+        
+        services.AddScoped<IJobRepository, JobRepository>();
+        services.AddSingleton<IMessageBroker, MqttMessageBroker>();
+        
+        return services;
+    }
+}
